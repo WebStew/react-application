a5f15fd6538f1bcd9e2c4abe7823b49593d2cab0
Pretorius <spretorius@wiley.com>
2017-01-25 10:27:17 +0000

    REACT | spretorius | - Added file-loader for CSS images

--- client/configuration/build/loader-file.js
+++ client/configuration/build/loader-file.js
@@ -1,12 +1,19 @@
 
 module.exports = {
-	test 	: /\.(jpe?g|png|gif|svg)$/i ,
-	loader 	: [
+	test : /.*\.(gif|png|jpe?g|svg)$/i,
+	loaders : [
+		'file-loader' ,
 		{
-			loader : 'file?hash=sha512&digest=hex&name=[hash].[ext]'
-		} ,
-		{
-			loader : 'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+			loader 	: 'image-webpack-loader' ,
+			query 	: {
+				progressive 		: true 	,
+				optimizationLevel 	: 7 	,
+				interlaced 			: false ,
+				pngquant 			: {
+					quality : '65-90' ,
+					speed 	: 4
+				}
+			}
 		}
 	]
 };
--- client/configuration/build/plugin-postcss.js
+++ client/configuration/build/plugin-postcss.js
@@ -5,7 +5,7 @@ module.exports 	= new webpack.LoaderOptionsPlugin({
 	options 	: {
 		postcss : [
 			require ( 'postcss-import' 				) () ,
-			require ( 'postcss-url' 				) () ,
+			//require ( 'postcss-url' 				) () ,
 			require ( 'postcss-cssnext'				) ,
 			require ( 'postcss-browser-reporter' 	) () ,
 			require ( 'postcss-reporter' 			) ()
